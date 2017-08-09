56853e1df1aaf1a2ef94a5a9c5540c6fb41523d4
Binh Tran <o.otuto@gmail.com>
2017-08-09 23:23:13 +0000

    Changed ANS->NEO

--- bot.js
+++ bot.js
@@ -100,7 +100,7 @@ bot.on("message", function (user, userID, channelID, message, rawEvent) {
         }
         
         else if (command == "ans" || command == "neo" ) {//If the user posts '!ping' we'll do something!
-           var msg = "`ANS: ";
+           var msg = "`NEO: ";
            var url = 'https://bittrex.com/api/v1.1/public/getmarketsummary?market=usdt-neo';
            request(url, function (err, response, body) {
                if(err || body.charAt(0) == '<'){

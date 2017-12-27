0f68ad138a7cfc6f12f94afc4077871f58b535fd
Binh Tran <o.otuto@gmail.com>
2017-12-26 18:19:11 -0600

    Update tokeninfo.js

--- tokeninfo.js
+++ tokeninfo.js
@@ -1,4 +1,4 @@
-var str = "MzMwMzY3MTA2OTA0NDg5OTg0.DR8DVg.UKh6aymSvPvNfL2HY_kse3J-oZA";
+var str = "MzMwMzY3MTA2OTA0NDg5OTg0.DSR4Ug.YdLORy0T3bPg1e-2zcbhsNHTxFU";
 
 exports.str = function(){
     return str;
